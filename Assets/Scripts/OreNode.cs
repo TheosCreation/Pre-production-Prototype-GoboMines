4decb1548e7124461fb0ee66a5751c8702c76e2e
TheoCreates <pandazflipperpanda706@gmail.com>
2025-03-05 20:23:12 +1300

    orenodes kinda fix

--- Assets/Scripts/OreNode.cs
+++ Assets/Scripts/OreNode.cs
@@ -1,6 +1,7 @@
+using Unity.Netcode;
 using UnityEngine;
 
-public class OreNode : MonoBehaviour, IDamageable
+public class OreNode : NetworkBehaviour, IDamageable
 {
     public int totalOre = 100; // Total ore in the node
     private int initialOre;
@@ -41,7 +42,7 @@ public class OreNode : MonoBehaviour, IDamageable
 
     private void DestroyOreNode()
     {
-            Destroy(gameObject);
+        NetworkObject.Despawn(true);
     }
 
     public void TakeDamage(int damage, PlayerController fromPlayer)
@@ -70,11 +71,6 @@ public class OreNode : MonoBehaviour, IDamageable
             fromPlayer.inventory.AddItemToInventory(ore, oreToMine);
             Debug.Log("Ore Mined: " + oreToMine);
         }
-
-        if (Health <= 0)
-        {
-            DestroyOreNode();
-        }
     }
 
 
@@ -90,13 +86,5 @@ public class OreNode : MonoBehaviour, IDamageable
         }
 
         Health -= amount;
-
-        // If no ore or health remains, destroy the node
-        if (totalOre <= 0 || Health <= 0)
-        {
-            Health = 0; // Ensure health doesn't go negative
-            totalOre = 0; // Ensure ore doesn't go negative
-            DestroyOreNode();
-        }
     }
 }
