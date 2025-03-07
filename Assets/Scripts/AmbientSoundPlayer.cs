cc0a0cbf2c83a54c34b717524ae8a57c1894cdd6
kristiandfrance <kristian.france@mds.ac.nz>
2025-03-07 20:51:56 +1300

    ambient audio fix

--- Assets/Scripts/AmbientSoundPlayer.cs
+++ Assets/Scripts/AmbientSoundPlayer.cs
@@ -3,26 +3,31 @@ using UnityEngine.Audio;
 
 public class AmbientSoundPlayer : MonoBehaviour
 {
-    [SerializeField] private AudioClip[] audioClip;
+    [SerializeField] private AudioClip[] audioClips;
 
     AudioSource audioSource;
 
+    private float lastPlayedTime = 0f;
 
     private void Awake()
     {
         audioSource = GetComponent<AudioSource>();
     }
 
-
     // Update is called once per frame
     void Update()
     {
-        if (((int)GameManager.Instance.timeOfDay % 20 == 1) && (1 - (GameManager.Instance.timeOfDay / (int)GameManager.Instance.timeOfDay) <= 0.01));
+        float timeOfDay = GameManager.Instance.timeOfDay;
+
+        // Check if 20-second intervals have passed since the last trigger
+        if (timeOfDay >= lastPlayedTime + 20f)
         {
-            if (UnityEngine.Random.Range(0, 100) <= 30 )
+            lastPlayedTime = timeOfDay; // Update the last trigger time
+
+            if (UnityEngine.Random.Range(0, 100) <= 30)
             {
-                // Play Random Sound
-                audioSource.resource = audioClip[UnityEngine.Random.Range(0, audioClip.Length)];
+                // Play a random sound
+                audioSource.clip = audioClips[UnityEngine.Random.Range(0, audioClips.Length)];
                 audioSource.Play();
             }
         }
