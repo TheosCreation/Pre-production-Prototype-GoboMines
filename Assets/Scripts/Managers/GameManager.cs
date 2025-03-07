f2dc44933e7d46c749f7984007366a6510da85fc
kristiandfrance <kristian.france@mds.ac.nz>
2025-03-07 18:25:41 +1300

    day changing and timer during day added in GameManager

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -13,4 +13,26 @@ public class GameManager : SingletonPersistent<GameManager>
     public GlobalPrefabs prefabs;
     public UnityEvent onHostEvent;
     public int day = 1;
+    public float timeOfDay = 0;
+    public bool isDayProgressing = false;
+
+    public void Update()
+    {
+        timeOfDay += Time.deltaTime;
+    }
+
+    public void StartDay()
+    {
+        timeOfDay = 0;
+        isDayProgressing = true;
+        // Start Spawning Of Enemies and Timer
+
+    }
+
+    public void EndDay()
+    {
+        isDayProgressing = false;
+        day++;
+        // Generator.CreateNewMap
+    }
 }
